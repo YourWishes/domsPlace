8b7d74aaf425bfa051831c5c830b0054e9b39d43
Dominic Masters <dominic@domsplace.com>
2018-07-11 07:33:39 +1000

    Images are now production worthy

--- private/server/WebpackCompilerOptions.js
+++ private/server/WebpackCompilerOptions.js
@@ -78,11 +78,23 @@ module.exports = function(server, app) {
       },
 
       {
-        test: /\.jpe?g$|\.gif$|\.png$|\.svg|\.webm|\.mp4$/i,
+        test: /\.svg|\.webm|\.mp4$/i,
         use: [{
           loader: "file-loader",
           options: {
             name: "[path][name].[ext]",
+            context: 'public'
+          }
+        }]
+      },
+
+      {
+        test: /\.jpe?g$|\.gif$|\.png$/i,
+        use: [{
+          loader: "responsive-loader",
+          options: {
+            sizes: [128, 256, 500, 750, 1000, 1250, 1500, 2000, 2250, 2500],
+            name: "[path][name]_[width]x.[ext]",
             context: 'public'
           }
         }]
