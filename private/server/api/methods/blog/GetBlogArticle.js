acdea924a2a7cdd2dc1fdb83ff6cac2eeef26526
Dominic Masters <dominic@domsplace.com>
2018-11-27 08:23:23 +1100

    Updated Webpack, built extremely basic blog insertion tool.

--- private/server/api/methods/blog/GetBlogArticle.js
+++ private/server/api/methods/blog/GetBlogArticle.js
@@ -21,10 +21,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-const
-  APIHandler = require('./../../APIHandler'),
-  sanitizeHtml = require('sanitize-html')
-;
+const APIHandler = require('./../../APIHandler');
 
 const ERRORS = {
   missingHandle: "Missing article handle.",
